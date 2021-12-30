fdf7bfdae045f14910421d154e894c70c69986b6
Alexandrescu Nicolae <nicu24dec.an@gmail.com>
2021-12-30 06:30:18 +0200

    finished the snake increasing size, score and game menu

--- main.py
+++ main.py
@@ -30,14 +30,14 @@ def draw_map(surface):
                 pygame.draw.rect(surface, (255, 255, 255), square2)
 
 
-def random_coordinates():
+def get_random_coordinates():
     return (random.randint(0, squares_per_width - 1) * square_size,
             random.randint(0, squares_per_height - 1) * square_size)
 
 
 class Target(object):
     def __init__(self):
-        self.position = random_coordinates()
+        self.position = get_random_coordinates()
         self.color = (255, 255, 0)
 
     def draw(self, surface):
@@ -50,13 +50,14 @@ class Target(object):
 class Snake(object):
     def __init__(self):
         self.length = 2
-        self.coordinates = [(width / 2, height / 2), (width / 2 - square_size, height / 2)]
+        self.coordinates = [((width - 2 * border_width) // 2, (height - 2 * border_width) // 2),
+                            ((width - 2 * border_width) - square_size, (height - 2 * border_width) // 2)]
         self.color = (0, 204, 0)
         self.direction = random.choice([north, east, south])
 
     def draw(self, surface):
         for position in self.coordinates:
-            square = pygame.Rect((position[0], position[1]), (square_size, square_size))
+            square = pygame.Rect((border_width + position[0], border_width + position[1]), (square_size, square_size))
             pygame.draw.rect(surface, self.color, square)
             pygame.draw.rect(surface, (255, 0, 0), square, 3)
 
@@ -66,7 +67,9 @@ class Snake(object):
                 pygame.quit()
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_UP:
+                if event.key == pygame.K_ESCAPE:
+                    return -1
+                elif event.key == pygame.K_UP:
                     self.set_direction(north)
                 elif event.key == pygame.K_RIGHT:
                     self.set_direction(east)
@@ -74,6 +77,7 @@ class Snake(object):
                     self.set_direction(south)
                 elif event.key == pygame.K_LEFT:
                     self.set_direction(west)
+        return 1
 
     def set_direction(self, direction):
         if (direction[0] * -1, direction[1] * -1) != self.direction:
@@ -82,42 +86,91 @@ class Snake(object):
     def get_head_coordinates(self):
         return self.coordinates[0]
 
-    def respawn(self):
-        self.length = 2
-        self.coordinates = [(width / 2, height / 2), (width / 2 - square_size, height / 2)]
-        self.direction = random.choice([north, east, south])
-
     def move(self):
         head_position = self.get_head_coordinates()
         next_position = (
             head_position[0] + self.direction[0] * square_size, head_position[1] + self.direction[1] * square_size)
 
-        if len(self.coordinates) > 2 and next_position in self.coordinates[2:] or next_position[0] < border_width or \
-                next_position[0] >= width - border_width or next_position[1] < border_width or \
-                next_position[1] >= height - border_width:
-            self.respawn()
+        if len(self.coordinates) > 2 and next_position in self.coordinates[2:] or next_position[0] < 0 or \
+                next_position[0] >= width - 2 * border_width or next_position[1] < 0 or \
+                next_position[1] >= height - 2 * border_width:
+            return -1
         else:
             self.coordinates.insert(0, next_position)
-            self.coordinates.pop()
+            if len(self.coordinates) > self.length:
+                self.coordinates.pop()
+        return 1
 
 
-def start_game():
+def start_game(high_score):
     pygame.init()
     clock = pygame.time.Clock()
     window = pygame.display.set_mode((width, height))
     surface = pygame.Surface(window.get_size())
     target = Target()
     snake = Snake()
+    score = 0
+    font = pygame.font.Font("Roboto-Medium.ttf", 20)
 
     while True:
         clock.tick(5)
-        snake.handle_keys()
+        exit_flag = snake.handle_keys()
+        if exit_flag == -1:
+            break
         draw_map(surface)
-        snake.move()
+        lost_flag = snake.move()
+        if lost_flag == -1:
+            break
+        if snake.get_head_coordinates() == target.position:
+            snake.length += 1
+            score += 1
+            target.position = get_random_coordinates()
         snake.draw(surface)
         target.draw(surface)
         window.blit(surface, (0, 0))
+        score_text = font.render("Score: {0}".format(score), True, (255, 0, 0))
+        window.blit(score_text, (10, 10))
+        high_score_text = font.render("High score: {0}".format(high_score), True, (255, 0, 0))
+        window.blit(high_score_text, (600, 10))
+        exit_text = font.render("Press 'ESC' to exit", True, (255, 0, 0))
+        window.blit(exit_text, (250, 10))
+        pygame.display.update()
+
+    return score
+
+
+def draw_score(window, font, score, high_score):
+    score_text = font.render("Score: {0}".format(score), True, (255, 128, 0))
+    window.blit(score_text, (width / 2 - 300, height / 2 - 100))
+    high_score_text = font.render("High score: {0}".format(high_score), True, (255, 128, 0))
+    window.blit(high_score_text, (width / 2 - 300, height / 2 - 150))
+    start_text = font.render("Press key 'Space' to start the game", True, (255, 128, 0))
+    window.blit(start_text, (width / 2 - 300, height / 2 - 50))
+
+
+def main():
+    pygame.init()
+    clock = pygame.time.Clock()
+    window = pygame.display.set_mode((width, height))
+    surface = pygame.Surface(window.get_size())
+    font = pygame.font.Font("Roboto-Medium.ttf", 40)
+    high_score = 0
+    score = 0
+    draw_score(window, font, score, high_score)
+    while True:
+        clock.tick(5)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    score = start_game(high_score)
+        if score > high_score:
+            high_score = score
+        window.blit(surface, (0, 0))
+        draw_score(window, font, score, high_score)
         pygame.display.update()
 
 
-start_game()
+main()
